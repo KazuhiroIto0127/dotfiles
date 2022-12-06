307f0149f0cd4cc73cead6d32d9e99bb6983f615
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2022-12-06 18:55:01 +0900

    aracrittyのalt設定をkarabiner elementで補うことで対応した

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -95,10 +95,10 @@ if (status) then
   -- alt押しながらhjkl
   -- altが<A>として認識されるようにするには、iTerm2の設定でProfiles>Keys>ESC+にする必要あり
 
-  vim.keymap.set('n', '<S-h>', require('smart-splits').resize_left)
-  vim.keymap.set('n', '<S-j>', require('smart-splits').resize_down)
-  vim.keymap.set('n', '<S-k>', require('smart-splits').resize_up)
-  vim.keymap.set('n', '<S-l>', require('smart-splits').resize_right)
+  vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+  vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+  vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+  vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
   -- moving between splits
   vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
   vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
